f70269a5c738acd4b124034b8addac189ca8795a
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-07-09 11:07:28 -0700

    responsive menu

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ $(document).ready(addHandlers);
 
 function addHandlers(){
   let element = $('nav ul li');
+  let button = $('.header--menutoggle');
+  let menu = $('.header--menu');
   element.on('click',function(e){
       $(this).toggleClass('active').siblings().removeClass('active');
       e.stopPropagation();
     }
   );
+  button.on('click', function(e) {
+    menu.toggleClass('show')
+    e.stopPropagation();
+  });
   $(window).on('click',function(e) {
     if(element.hasClass('active')){
       element.removeClass('active');
+    }
+    if(menu.hasClass('show')){
+      menu.removeClass('show');
     }
   });
 }
